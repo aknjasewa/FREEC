efd132902cf406278a0b0024716628076861478a
aknjasewa <knjasewa1nastja@gmail.com>
2023-12-12 15:40:08 +0300

    Del -q 1

--- src/BAFpileup.cpp
+++ src/BAFpileup.cpp
@@ -298,7 +298,7 @@ std::string BAFpileup::createPileUpFile(std::string outputDir, std::string samto
 
     string command;
     if (pathToSambamba != "")    {
-        string samtools_arg = "--samtools -f " +fastaFile+ " -d 8000 -Q "+int2string(minQualPerPos)+ " -q 1 -l " + intersected;
+        string samtools_arg = "--samtools -f " +fastaFile+ " -d 8000 -Q "+int2string(minQualPerPos)+ " -l " + intersected;
         command  = pathToSambamba + " mpileup -t " + SambambaThreads + " -o " + minipileup + " " + control_MateFile + " " + samtools_arg ;
     }  else   {
          command = samtools_path + " mpileup -f "+fastaFile+" -d 8000 -Q "+int2string(minQualPerPos)+" -q 1 -l " + intersected + " " + control_MateFile + " > " + minipileup; //discard reads wit 0 mapping quality
