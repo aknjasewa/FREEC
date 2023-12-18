4af3212f990e0169907090a68b9fd7e83099fdbc
aknjasewa <101805838+aknjasewa@users.noreply.github.com>
2023-12-18 15:15:23 +0300

    Update BAFpileup.cpp

--- src/BAFpileup.cpp
+++ src/BAFpileup.cpp
@@ -301,7 +301,7 @@ std::string BAFpileup::createPileUpFile(std::string outputDir, std::string samto
         string samtools_arg = "--samtools -f " +fastaFile+ " -d 8000 -Q "+int2string(minQualPerPos)+ " -l " + intersected;
         command  = pathToSambamba + " mpileup -t " + SambambaThreads + " -o " + minipileup + " " + control_MateFile + " " + samtools_arg ;
     }  else   {
-         command = samtools_path + " mpileup -f "+fastaFile+" -d 8000 -Q "+int2string(minQualPerPos)+" -l " + intersected + " " + control_MateFile + " > " + minipileup; //discard reads wit 0 mapping quality
+         command = pathToSambamba + " mpileup -f "+fastaFile+" -d 8000 -Q "+int2string(minQualPerPos)+" -l " + intersected + " " + control_MateFile + " > " + minipileup; //discard reads wit 0 mapping quality
     }
 	
     #include <iostream>
